d75af7ee8c573b40e0eaae56fffcf61b82dd1bb5
Thinh Nguyen <thanhthinhcntt@gmail.com>
2018-08-20 23:39:32 +0700

    add blogdbcontext

---
+++ Blog/Blog.EntityFrameworkCore/BlogDbContext.cs
@@ -0,0 +1,28 @@
+﻿using Blog.Core.Domains;
+using Blog.Core.Domains.Identities;
+using Blog.EntityFrameworkCore.Mapping;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Blog.EntityFrameworkCore
+{
+    public class BlogDbContext : IdentityDbContext<User, Role, long, UserClaim, UserRole, UserLogin, RoleClaim, UserToken>
+    {
+        public BlogDbContext(DbContextOptions<BlogDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Post> Posts { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.ApplyConfiguration(new PostMap());
+            // etc....
+
+        }
+    }
+
+}
---
+++ Blog/Blog.EntityFrameworkCore/Mapping/PostMap.cs
@@ -0,0 +1,15 @@
+﻿using Blog.Core.Domains;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Blog.EntityFrameworkCore.Mapping
+{
+    public class PostMap: IEntityTypeConfiguration<Post>
+    {
+        public void Configure(EntityTypeBuilder<Post> builder)
+        {
+            builder.ToTable("Post");
+            builder.HasKey(n => n.Id);
+        }
+    }
+}
