03b32b1f474b62f8c5b6aa135d3f8574449e02d1
Thinh Nguyen <thanhthinhcntt@gmail.com>
2018-08-20 23:17:32 +0700

    add post

---
+++ Blog/Blog.Core/Domains/Post.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Blog.Core.Domains
+{
+    public class Post : Entity<long>
+    {
+        
+        public string Title { get; set; }
+
+        public string Permalink { get; set; }
+
+        public string Description { get; set; }
+
+        public string Content { get; set; }
+
+        public int Status { get; set; }
+
+        public long CreatedBy { get; set; }
+
+        public DateTime CreatedDate { get; set; } = DateTime.Now;
+
+        public long ModifiedBy { get; set; }
+
+        public DateTime ModifiedDate { get; set; } = DateTime.Now;
+    }
+}
