a6ea6354be56d7118d5e3b8edddb627f36b3ab9a
thanhthinhcntt <thanhthinhcntt@gmail.com>
2018-10-18 14:24:20 +0700

    add layout admin

---
+++ Blog/Blog.Common/Constants/WebConstants.cs
@@ -0,0 +1,12 @@
+﻿namespace Blog.Common.Constants
+{
+    public class WebConstants
+    {
+        public class Routing
+        {
+            public const string Admin = "Admin";
+
+            public const string AdminCreateUser = "Admin/Users/Create";
+        }
+    }
+}
---
+++ Blog/Blog.Web/Areas/Admin/Controllers/UsersController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Blog.Common.Constants;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Blog.Web.Areas.Admin.Controllers
+{
+    using static WebConstants;
+    [Area("Admin")]
+    public class UsersController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        [Route(Routing.AdminCreateUser)]
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
