14dc78e3c1dc213d72948fbf8cb5febe5f38e847
thanhthinhcntt <thanhthinhcntt@gmail.com>
2018-10-18 15:25:28 +0700

    add dbfactory

--- Blog/Blog.EntityFrameworkCore/Extensions/BlogDbContextFactory.cs
+++ Blog/Blog.EntityFrameworkCore/Extensions/BlogDbContextFactory.cs
@@ -1,10 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
 
 namespace Blog.EntityFrameworkCore.Extensions
 {
-    class BlogDbContextFactory
+    public class BlogDbContextFactory : IDesignTimeDbContextFactory<BlogDbContext>
     {
+        public BlogDbContext CreateDbContext(string[] args)
+        {
+            var builder = new DbContextOptionsBuilder<BlogDbContext>();
+
+            builder.UseSqlServer("Server=NTTHINH-PC\\SQL2K14;Database=Blog;Trusted_Connection=True;MultipleActiveResultSets=true");
+            return new BlogDbContext(builder.Options);
+        }
     }
 }
